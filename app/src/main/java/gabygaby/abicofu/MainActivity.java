c7535aa7002c1ab4cb93680d6d0f36bdc101930f
Rémi Pannequin <remi.pannequin@univ-lorraine.fr>
2015-07-06 09:28:42 +0200

    use Abicofu font in buttons and textviews

--- app/src/main/java/gabygaby/abicofu/MainActivity.java
+++ app/src/main/java/gabygaby/abicofu/MainActivity.java
@@ -1,9 +1,13 @@
 package gabygaby.abicofu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Abicofu.ttf");
+        for (int b : new int[]{R.id.button0, R.id.button1, R.id.button2, R.id.button3,
+                R.id.button4, R.id.button5, R.id.button6, R.id.button7,
+                R.id.button8, R.id.button9, R.id.buttonA, R.id.buttonB,
+                R.id.buttonC, R.id.buttonD, R.id.buttonE, R.id.buttonF}) {
+            Button button = (Button) findViewById(b);
+            button.setTypeface(font);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Button b = (Button) v;
+                    CharSequence text = b.getText();
+                    addFigure(text.charAt(0));
+                }
+            });
+
+        }
+
+        TextView tv = (TextView) findViewById(R.id.textViewResultAbicufo);
+        tv.setTypeface(font);
+    }
+
+    /**
+     * Add a figure to the nuber displayed
+     * @param c the char to append
+     */
+    private void addFigure(char c) {
+        TextView tv = (TextView) findViewById(R.id.textViewResultAbicufo);
+        CharSequence old_t = tv.getText();
+        String new_t = String.format("%s%s", old_t, c);
+        tv.setText(new_t);
+    }
+
+
+    private void update() {
+
+
+
     }
 
     @Override
