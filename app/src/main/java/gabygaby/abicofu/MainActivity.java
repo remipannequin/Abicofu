6b4e4c496a7ba168858cbe30946fc37960b7f89c
Rémi Pannequin <remi.pannequin@univ-lorraine.fr>
2015-07-06 14:26:14 +0200

    better layout

--- app/src/main/java/gabygaby/abicofu/MainActivity.java
+++ app/src/main/java/gabygaby/abicofu/MainActivity.java
@@ -11,47 +11,64 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    long number = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Abicofu.ttf");
+        int i = 0;
         for (int b : new int[]{R.id.button0, R.id.button1, R.id.button2, R.id.button3,
                 R.id.button4, R.id.button5, R.id.button6, R.id.button7,
                 R.id.button8, R.id.button9, R.id.buttonA, R.id.buttonB,
                 R.id.buttonC, R.id.buttonD, R.id.buttonE, R.id.buttonF}) {
             Button button = (Button) findViewById(b);
             button.setTypeface(font);
+            button.setTag(i++);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Button b = (Button) v;
-                    CharSequence text = b.getText();
-                    addFigure(text.charAt(0));
+                    int number = (Integer)v.getTag();
+                    addFigure(number);
                 }
             });
-
         }
 
-        TextView tv = (TextView) findViewById(R.id.textViewResultAbicufo);
+        TextView tv = (TextView) findViewById(R.id.textViewResultAbicofu);
         tv.setTypeface(font);
+        update();
     }
 
     /**
      * Add a figure to the nuber displayed
      * @param c the char to append
      */
-    private void addFigure(char c) {
-        TextView tv = (TextView) findViewById(R.id.textViewResultAbicufo);
-        CharSequence old_t = tv.getText();
-        String new_t = String.format("%s%s", old_t, c);
-        tv.setText(new_t);
+    private void addFigure(int c) {
+        number = number * 16 + c;
+        update();
     }
 
 
-    private void update() {
+    public void clear(View button) {
+        number = 0;
+        update();
+    }
 
+    private void update() {
+        StringBuilder hex_repr = new StringBuilder(String.format("%08X", number));
+        for (int i = hex_repr.length(); i >= 0; i = i-2){
+            hex_repr.insert(i, " ");
+        }
+        TextView tv1 = (TextView) findViewById(R.id.textViewResultAbicofu);
+        tv1.setText(hex_repr);
+        TextView tv3 = (TextView) findViewById(R.id.textViewResultHexa);
+        tv3.setText(hex_repr);
+
+        String text_repr = new AbicofuFormatter(number).write();
+        TextView tv2 = (TextView) findViewById(R.id.textViewResultText);
+        tv2.setText(text_repr);
 
 
     }
@@ -77,4 +94,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
